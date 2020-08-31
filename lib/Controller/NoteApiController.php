570e77dc77293e946d58e8aa79b62f25065dd017
Matias De lellis <mati86dl@gmail.com>
2020-08-31 15:12:23 -0300

    Fix update method on Api controller

--- lib/Controller/NoteApiController.php
+++ lib/Controller/NoteApiController.php
@@ -119,10 +119,11 @@ class NoteApiController extends ApiController {
 	 * @param array $attachts
 	 * @param bool $pinned
 	 * @param array $tags
+	 * @param array $shared_with
 	 * @param string $color
 	 */
-	public function update(int $id, string $title, string $content, array $attachts, bool $pinned, array $tags, string $color = "#F7EB96"): JSONResponse {
-		$note = $this->noteService->update($this->userId, $id, $title, $content, $attachts, $pinned, $tags, $color);
+	public function update(int $id, string $title, string $content, array $attachts, bool $pinned, array $tags, array $shared_with, string $color = "#F7EB96"): JSONResponse {
+		$note = $this->noteService->update($this->userId, $id, $title, $content, $attachts, $pinned, $tags, $shared_with, $color);
 		if (is_null($note)) {
 			return new JSONResponse([], Http::STATUS_NOT_FOUND);
 		}
