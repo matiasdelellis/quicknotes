3ebbbb0207a3ca9b54568d80d7ac3cce05dccc4f
Matias De lellis <mati86dl@gmail.com>
2020-10-27 15:15:36 -0300

    Update translations.

---
+++ l10n/pt_BR.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "quicknotes",
+    {
+    "Quick notes" : "Notas rápidas",
+    "Cancel" : "Cancelar",
+    "Done" : "Concluído",
+    "Tags" : "Tags",
+    "Save" : "Salvar",
+    "All notes" : "Todas as notas",
+    "Colors" : "Cores",
+    "Bold" : "Negrito",
+    "Italic" : "Itálico",
+    "Underline" : "Sublinhado"
+},
+"nplurals=2; plural=(n > 1);");
