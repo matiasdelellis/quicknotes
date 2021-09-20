443f6bf14432636c4c8322efa2a73f62a7e330aa
Matias De lellis <mati86dl@gmail.com>
2021-09-20 19:52:13 -0300

    Explain the methods of the NoteShare entity and clean..

--- lib/Db/NoteShare.php
+++ lib/Db/NoteShare.php
@@ -5,17 +5,23 @@ use JsonSerializable;
 
 use OCP\AppFramework\Db\Entity;
 
+/**
+ * @method int getNoteId()
+ * @method void setNoteId(int $noteId)
+ * @method string getSharedUser()
+ * @method void setSharedUser(string $userId)
+ * @method string getSharedGroup()
+ * @method void setSharedGroup(string $groupId)
+ */
 class NoteShare extends Entity implements JsonSerializable {
 
 	protected $noteId;
-	protected $userId;
 	protected $sharedUser;
 	protected $sharedGroup;
 
 	public function jsonSerialize() {
 		return [
 			'id' => $this->id,
-			'user_id' => $this->userId,
 			'note_id' => $this->noteId,
 			'shared_user' => $this->sharedUser,
 			'shared_group' => $this->sharedGroup
